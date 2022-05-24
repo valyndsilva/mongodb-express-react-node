e572c076a25dcd92a0d6a3394dc27b09df87ec0e
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-05-24 03:15:33 +0100

    event emitter

--- npm-modules-event-emitter/index.js
+++ npm-modules-event-emitter/index.js
@@ -1,6 +1,16 @@
-// console.log('testing!');
-const { format } = require('date-fns');
-const { v4: uuid } = require('uuid');
-console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss')); // https://date-fns.org/
-console.log('Test');
-console.log(uuid()); // https://www.npmjs.com/package/uuid
+const logEvents = require('./logEvents');
+
+// common core module 'events'
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
+
+// initialize object
+const myEmitter = new MyEmitter();
+// add listener for log event
+myEmitter.on('log', (msg) => logEvents(msg)); // .on listens event
+
+setTimeout(() => {
+  // Emit event
+  myEmitter.emit('log', 'Log event emitted');
+}, 2000);
